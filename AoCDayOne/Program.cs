652ca65192854a534366d109b0738fcc7ae1a731
Maxw_ll <ma.bilang0023@gmail.com>
2025-07-18 22:02:14 +0100

    Stored left and right numbers to different lists
    Using .SubString on a made string

--- AoCDayOne/Program.cs
+++ AoCDayOne/Program.cs
@@ -18,16 +18,24 @@ namespace AoCDayOne
                 line = sr.ReadLine();
 
                 List<string> lines = new List<string>();
+                List<string> linesleft = new List<string>();
+                List<string> linesright = new List<string>();
 
                 while (line != null)
                 {
+                    string left = line.Substring(0, 5);
+                    linesleft.Add(left);
+                    string right = line.Substring(8);
+                    linesright.Add(right);
+
                     lines.Add(line);
+
                     line = sr.ReadLine();
                 }
 
                 for (int i = 0; i < lines.Count; i++)
                 {
-                    Console.WriteLine(lines[i]);
+                    Console.WriteLine(linesleft[i] + linesright[i]);
                 }
 
                 sr.Close();
